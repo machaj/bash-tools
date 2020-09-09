6f33688d9539f75dd3b83f71f56369d0b9a1e69a
Jan Machacek <jan.machacek@topmonks.com>
2020-09-09 13:02:56 +0200

    Fix bitcoin value scraping

--- crypto-info.sh
+++ crypto-info.sh
@@ -5,15 +5,16 @@
 #        crypto_exchange_rate COIN_NAME
 #
 # DESCRIPTION
-#        Grep currency exchange rate from coinmarketcap.
+#        Grep currency exchange rate from coinmarketcap and save value
+#        into hidden file at home folder
 #
 #        crypto_exchange_rate bitcoin
 
 crypto_exchange_rate() {
   local url="https://coinmarketcap.com/currencies/$1/"
-  local grep_expr="details-panel-item--price__value\" data-currency-value>[0-9]+\.[0-9]+"
+  local grep_expr="\"@type\":\"Offer\",\"price\":[0-9]+\.[0-9]{2}"
   local filename=~/.$1_value
   
-  curl -s $url | grep -o -E "$grep_expr" | cut -c 55- > $filename 
+  curl -s $url | grep -o -E "$grep_expr" | cut -c 25- > $filename 
 }
 
